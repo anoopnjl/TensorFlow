a0cd3b16376abe8c74574c7c72728eba178c517f
anoop <anoop_a@infosys.com>
2018-12-20 23:26:09 +0100

    basic tensorflow coding2

---
+++ 01.Simple Linear Model with MNIST.py
@@ -0,0 +1,47 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+    
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i], cmap='binary')
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        ax.set_xlabel(xlabel)
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+	
+
+# Get the first images from the test-set.
+images = x_test[0:9] * 255.0
+
+# Get the true classes for those images.
+cls_true = y_test[0:9]
+
+# Plot the images and labels using our helper-function above.
+plot_images(images=images, cls_true=cls_true)	
+
+
